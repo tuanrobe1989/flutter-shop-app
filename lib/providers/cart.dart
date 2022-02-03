1de5fd0b77154b53ce7af43f84917c229fb25e29
luu.hoait <luu.hoait@gmail.com>
2022-02-03 12:16:58 +0700

    backup

---
+++ lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required String id,
+    @required String title,
+    @required int quantity,
+    @required double price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            id: DateTime.now().toString(),
+            title: title,
+            price: price,
+            quantity: 1),
+      );
+    }
+  }
+}
